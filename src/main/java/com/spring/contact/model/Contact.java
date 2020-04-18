8c1356d301dab10c9a5cec1b2449eeace6437c2a
chien <minhchiendc@gmail.com>
2020-04-18 20:18:14 +0700

    Cấu hình model & JPA
    1. Tạo model Contact trong package model
    2. Cấu hình entity cho Contact
    3. Cấu hình persistence.xml
    4. Trong file spring-mvc-servlet.xml thiết lập cấu hình JPA cho Spring

---
+++ src/main/java/com/spring/contact/model/Contact.java
@@ -0,0 +1,69 @@
+package com.spring.contact.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contact")
+public class Contact {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String firstName;
+	private String lastName;
+	private boolean gender;
+	private String email;
+	private String phone;
+	public Contact() {
+		super();
+	}
+	public Contact(String firstName, String lastName, boolean gender, String email, String phone) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.email = email;
+		this.phone = phone;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
